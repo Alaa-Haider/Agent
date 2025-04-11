21daec42e2e02db78c326658a6adaba094365244
Alaa Haider <alaahaider888@gmail.com>
2025-04-11 03:35:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,6 @@ if __name__ == "__main__":
     # Load configuration from environment variables
     debug_mode = os.getenv("DEBUG", "True").lower() == "true"
     host = os.getenv("HOST", "127.0.0.1")
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8080))
 
     app.run(debug=debug_mode, host=host, port=port)
